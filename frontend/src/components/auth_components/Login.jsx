ae3d9ced776d717c3536003c664ef51e441780f2
ltfrxx <lutfir.rn@gmail.com>
2024-07-27 21:27:24 +0800

    Fixed  Signup.css

--- frontend/src/components/auth_components/Login.jsx
+++ frontend/src/components/auth_components/Login.jsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
 import axios from "axios";
-import "./Login.css"
+import "./Login.css";
 
 const Login = (props) => {
     const navigate = useNavigate();
